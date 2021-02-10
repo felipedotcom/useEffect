37be43962306717cde5b28d60eba1a0b3c001c20
felipedotcom <felipe.rodolfo29@gmail.com>
2021-02-10 14:48:41 -0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    alert(`checked: ${checked.toString()}`);
+  });
+
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ transform: "scale(4)", minHeight: '100vh' }}
+      >
+        <Checkbox
+          checked={checked}
+          onChange={() => setChecked(checked => !checked)}
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+        />
+        {checked ? "checked" : "not checked"}
+      </Grid>
+    </>
+  );
+  
+}
 export default App;
